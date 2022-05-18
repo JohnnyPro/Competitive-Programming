d037603c9d0206fb4d16c554195f88ee9d444e20
Yohannes D. Nedaw <39728442+JohnnyPro@users.noreply.github.com>
2022-05-18 13:52:04 +0300

    Solution to Grading students problem

---
+++ grading_students.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+def rounded(n):
+    
+    if n < 38 or n % 5 == 0:
+        return n
+    
+    if n % 5 >= 3:
+        return (n//5 + 1) * 5
+    
+    else:
+        return n
+def gradingStudents(grades):
+    roundedGrades = []
+    for item in grades:
+        roundedGrades.append(rounded(item))
+    return roundedGrades
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
