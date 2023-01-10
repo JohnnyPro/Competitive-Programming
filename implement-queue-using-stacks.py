53056f203c715337d410dceb9376c7f7a86d2b65
Yohannes D. Nedaw <39728442+JohnnyPro@users.noreply.github.com>
2023-01-10 21:49:00 +0300

    Create implement-queue-using-stacks.py

---
+++ implement-queue-using-stacks.py
@@ -0,0 +1,28 @@
+class MyQueue:
+
+    def __init__(self):
+        self.queue = []
+
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+
+    def pop(self) -> int:
+        # Could've just said "return self.queue.pop(0)" but was not sure if that was allowed 
+        if self.empty(): return None
+        value = self.peek()
+        self.queue = self.queue[1:]
+        return value
+
+    def peek(self) -> int:
+        return self.queue[0]
+
+    def empty(self) -> bool:
+        return not bool(len(self.queue))
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
